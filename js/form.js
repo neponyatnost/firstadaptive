312865c364f0373410df45a59d5e5a722a7220c8
Ivan <iv.konuhov@gmail.com>
2022-07-07 15:59:28 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -12,25 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
         // let formData = new FormData(form);
         // formData.append('image', formImage.files[0]);
         
-        if (error === 0) {
-            form.classList.add('_sending');
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData
-            });
-            if (response.ok) {
-                let result = await response.json();
-                alert(result.message);
-                formPreview.innerHTML = '';
-                form.reset();
-                form.classList.remove('_sending');
-            } else {
-                alert('Ошибка!');
-                form.classList.remove('_sending');
-            }
-        } else {
-            alert('Заполните обязательные поля')
-        }
+        // if (error === 0) {
+        //     form.classList.add('_sending');
+        //     let response = await fetch('sendmail.php', {
+        //         method: 'POST',
+        //         body: formData
+        //     });
+        //     if (response.ok) {
+        //         let result = await response.json();
+        //         alert(result.message);
+        //         formPreview.innerHTML = '';
+        //         form.reset();
+        //         form.classList.remove('_sending');
+        //     } else {
+        //         alert('Ошибка!');
+        //         form.classList.remove('_sending');
+        //     }
+        // } else {
+        //     alert('Заполните обязательные поля')
+        // }
     }
 
 
